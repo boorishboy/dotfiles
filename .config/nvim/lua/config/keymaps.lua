0c9010d26b5fd36ca36baf046b2ae23c37073916
Wojciech Chamski <wojciech.chamski@stxnext.pl>
2024-02-25 13:13:56 +0100

    New dotfiles

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+local expr_opts = { noremap = true, expr = true, silent = true }
+
+keymap("n", "<Leader>d", '"_d', default_opts)
+keymap("x", "<Leader>d", '"_d', default_opts)
---
+++ .config/nvim/lua/plugins/my_plugins.lua
@@ -0,0 +1,14 @@
+return {
+  -- add LaTeX support
+  {
+    "lervag/vimtex",
+  },
+  -- add vim surround
+  {
+    "tpope/vim-surround",
+  },
+  -- add vim noneckpain
+  {
+    "shortcuts/no-neck-pain.nvim",
+  },
+}
